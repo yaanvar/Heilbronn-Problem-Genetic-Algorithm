a0efecc8b1223197e63d07d8a55fcee4db4256bc
Anvar <54923760+yaanvar@users.noreply.github.com>
2022-03-30 20:44:47 +0300

    NeoGeneticAlgorithm init

---
+++ NeoGeneticAlgorithm/NeoGeneticAlgorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  NeoGeneticAlgorithm
+//
+//  Created by Anvar Rahimov on 30.03.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
